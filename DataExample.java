d401fa1de6973114e7efd26121569e85e62a858a
krzkro4122 <krolkrzysztof1999@gmail.com>
2022-11-21 19:56:15 +0100

    Checkpoint

--- DataExample.java
+++ DataExample.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class DataExample implements Data {
     // Fields
     private int dataID;
-    private int[] data = new int[8];
+    private List<Integer> data;
     // Methods
-    DataExample() {data = (new int[] {1, 2, 3, 4, 5, 6, 7, 8}).clone(); dataID=0;}
-    DataExample(int[] data, int dataID) { this.data = data; this.dataID = dataID; }
+    DataExample() {dataID=0;}
+    DataExample(List<Integer> data, int dataID) { this.data = data; this.dataID = dataID; }
     @Override
     public int getDataId() { return dataID; }
     @Override
-    public int getSize() { return data.length; }
+    public int getSize() { return data.size(); }
     @Override
-    public int getValue(int idx) { return data[idx]; }
+    public int getValue(int idx) { return data.get(idx); }
 }
--- ParallelCalculator.java
+++ ParallelCalculator.java
@@ -1,36 +1,24 @@
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.PriorityBlockingQueue;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Callable;
 
-import java.util.NoSuchElementException;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Comparator;
-import java.util.AbstractQueue;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 class ParallelCalculator implements DeltaParallelCalculator {
     // Fields
-    List<Data> dataContainer = new ArrayList<Data>();
     private int threads = 0;
     private DeltaReceiver deltaReceiver;
-    private ThreadPoolExecutor executorService;
-    private PriorityBlockingQueue<Runnable> taskQueue;
+    private ExecutorService executorService;
+    protected List<Data> dataContainer = new ArrayList<Data>();
+    protected List<Integer> calculatedEntries = new ArrayList<Integer>();
     // CompletionService<ArrayList<Delta>> completionService;
 
     // Methods
-    ParallelCalculator() {
-        // Initialize the continuous receiver function
-        consumeQueue();
-    }
-
     @Override
     public void setThreadsNumber(int threads) {
         this.threads = threads;
@@ -46,113 +34,143 @@ class ParallelCalculator implements DeltaParallelCalculator {
     @Override
     public void setDeltaReceiver(DeltaReceiver receiver) { deltaReceiver = receiver; }
 
+    private <T> String formatCollection(Collection<T> collection) {
+        String output = new String();
+        output += "[";
+        for (T element : collection) {
+            output += element + ", ";
+        }
+        output += "] - " + collection.size();
+        return output;
+    }
+
     @Override
     public void addData(Data data) {
+        System.out.println("{" + Thread.currentThread() + "} " +"[PREV] dataContainer: " + formatCollection(dataContainer));
         dataContainer.add(data);
+        dataContainer.sort(Comparator.comparingInt(Data::getDataId));
+        System.out.println("{" + Thread.currentThread() + "} " +"[NOW] dataContainer: " + formatCollection(dataContainer));
 
-        // TODO ???
+        final List<SimpleEntry<Data, Data>> entries = new ArrayList<SimpleEntry<Data, Data>>();
 
-        System.out.println("[Queue] Added data: ID -> " + data.getDataId());
-    }
+        // Find consecutively numbered data IDs
+        Data firstData = dataContainer.get(0);
+        for (Data secondData : dataContainer ) {
 
-    /*
-    *   Continuously poll if there are at least two elements in the queue.
-    *   If there are at least 2 elements - create comparison tasks for them.
-    *   The Data pairs with the lowest IDs are consumed first.
-    */
-    public void consumeQueue() {
-        // Set to be done
-        // Custom Kolejka ktora
-        try {
-            System.out.println("[" + Thread.currentThread() + "] Consuming! PriorityQueue[len=" + taskQueue.size() + "]: " + taskQueue);
-            if ( !taskQueue.isEmpty() ) {
-                Task task = (Task) taskQueue.remove();
+            int desiredID = firstData.getDataId() + 1;
+            int actualID = secondData.getDataId();
 
-                // No dissagregatrion just yet! >:D
-                // TODO - queue parsing and Data set dissagregation stuff woof woof
+            System.out.println("{" + Thread.currentThread() + "} " +"dataIDs: " + desiredID + ", " + actualID);
 
-                System.out.println("[consumeQueue] Created a task: " + task + " - Submitting and getting...");
-                // Future<ArrayList<Delta>> response = completionService.submit(task);
+            if (desiredID ==  actualID) {
+                System.out.println("{" + Thread.currentThread() + "} " +"Adding matching DataIDs: " + desiredID + ", " + actualID);
+                entries.add(new SimpleEntry<Data, Data>(firstData, secondData));
+            }
+            firstData = secondData;
+        }
+        System.out.println("{" + Thread.currentThread() + "} " +"entries: " + formatCollection(entries));
+
+        entries.forEach(entry -> {
+            System.out.println("{" + Thread.currentThread() + "} " +"entry(DataIds): (" + entry.getKey().getDataId() + ", " + entry.getValue().getDataId() + ")");
+
+            List<Task> tasks = disaggregateIntoTasks(entry);
+
+            tasks.forEach(task -> {
+                System.out.println("{" + Thread.currentThread() + "} " +"taskID: " + task.acquireID());
                 executorService.execute(task);
-                deltaReceiver.accept(); // ???
-                // System.out.println("[consumeQueue] Submitted a task:  Taking...");
-                // response = completionService.take();
-
-                // System.out.println("[consumeQueue] Task response: " + response.get());
-                // synchronized (this) { deltaReceiver.accept(response.get()); }
-            }
-        } catch (NoSuchElementException nsee) {
-            System.out.println("NoSuchElementException caught! - " + nsee.getStackTrace());
-        } catch (InterruptedException ie) {
-            System.out.println("InterruptedException caught! - " + ie.getStackTrace());
-            return;
-        } catch (ExecutionException ee) {
-            System.out.println("ExecutionException caught! - " + ee.getStackTrace());
-        } catch (RejectedExecutionException ree) {
-            System.out.println("Rejected??? " + ree.getStackTrace()); // TODO ???
-        }
+            });
+        });
     }
 
-    // Create a task that compares the consequent data values and returns a list of discrepant data's indices
-    private Task createTask(final SimpleEntry<Data, Data> dataEntry) {
-        System.out.println("[createTask] Starting...");
-        return new Task(dataEntry.getKey(), dataEntry.getValue());
-    };
+    private List<Task> disaggregateIntoTasks(SimpleEntry<Data, Data> dataEntry) {
+        List<Task> tasks = new ArrayList<Task>();
 
-    private class Task implements Runnable {
-        // Fields
-        final Data firstData;
-        final Data secondData;
-        // Methods
-        Task(SimpleEntry<Data,Data> dataEntry) {
-            firstData = dataEntry.getKey();
-            secondData = dataEntry.getValue();
+        // All tasks have the same length
+        int dataSize = dataEntry.getKey().getSize();
+
+        for (int i = 0; i < threads; i++) {
+            tasks.add(new Task(dataEntry, i));
         }
-        // This does all the dirty work AKA computations
-        @Override
-        public void run() {
-            System.out.println("[call] Starting...");
-            ArrayList<Delta> badIndeces = new ArrayList<Delta>();
-            // Compare consequent data values.
-            // Assumption: both data sets are of the same size
-            for (int i = 0; i < firstData.getSize(); i++) {
-                System.out.println("[call] i: " + i);
-                int value1 = firstData.getValue(i);
-                int value2 = secondData.getValue(i);
-                // Add the resulting Delta created from the missmatched data values and index to the list
-                if ( value1 != value2 ) { badIndeces.add(new Delta(firstData.getDataId(), i, value1 - value2)); }
-            }
-        }
-    };
+
+        return tasks;
+    }
 
     private class TaskQueue extends PriorityBlockingQueue<Runnable> {
         public TaskQueue(int capacity) {
             super(capacity, new Comparator<Runnable>() {
                 @Override
                 public int compare(Runnable firstTask, Runnable secondTask) {
-                    return Integer.compare(
-                        ((Task)firstTask).firstData.getDataId(),
-                        ((Task)(secondTask)).secondData.getDataId()
-                    );
+                    return Comparator.comparingInt(Task::acquireID).compare((Task) firstTask, (Task) secondTask);
                 }
             });
         }
     }
 
+    private class Task implements Runnable {
+        // Fields
+        private final int id;
+        private final int startFrom;
+        private final Data firstData;
+        private final Data secondData;
+        private final List<Delta> deltas = new ArrayList<Delta>();
+
+        // Methods
+        Task(SimpleEntry<Data, Data> dataEntry, int start) {
+            startFrom = start;
+            firstData = dataEntry.getKey();
+            secondData = dataEntry.getValue();
+            id = firstData.getDataId();
+        }
+
+        // This does all the dirty work AKA computations
+        @Override
+        public void run() {
+            int dataSize = firstData.getSize();
+            for (int i = startFrom; i < dataSize; i += threads) {
+
+                int firstValue = firstData.getValue(i);
+                int secondValue = secondData.getValue(i);
+
+                if ( firstValue != secondValue ) {
+                    System.out.println("{" + Thread.currentThread() + "} " +"Adding deltaID: " + id + ", delta: " + (firstValue - secondValue));
+                    deltas.add(new Delta(id, i, firstValue - secondValue));
+                }
+            }
+
+            synchronized (dataContainer) {
+                deltaReceiver.accept(deltas);
+
+                // for (Data data : dataContainer) {
+                //     if (data.getDataId() == ID)
+                //         dataContainer.remove(data);
+                // }
+            }
+        }
+
+        public int acquireID() { return id; }
+    }
+
     // Main
     public static void main(String[] args) {
         ParallelCalculator pc = new ParallelCalculator();
-        pc.setThreadsNumber(4);
-        pc.setDeltaReceiver(
-            new DeltaReceiver() {
-                @Override
-                public void accept(List<Delta> deltas) { System.out.println("ziomal..."); }
+        pc.setThreadsNumber(2);
+        pc.setDeltaReceiver(new DeltaReceiver() {
+            @Override
+            public void accept(List<Delta> deltas) {
+                System.out.print("{" + Thread.currentThread() + "} " + "Accepting deltas: [");
+                for (Delta element : deltas) {
+                    System.out.print (element.getDataID() + ", ");
+                }
+                System.out.println("] -" + deltas.size());
             }
-        );
+        });
 
-        for (int i = 0; i < 20; i++) {
-            int[] arr = new int[8];
-            for (int j = 0; j < 8; j++) { arr[j] = j + i; }
+        int data_size = 4;
+        for (int i = 0; i < data_size; i++) {
+            List<Integer> arr = new ArrayList<Integer>();
+            for (int j = 0; j < data_size; j++)
+                arr.add(j - i * j);
+            System.out.println("{" + Thread.currentThread() + "} " +"Data[" + i + "]: " + pc.formatCollection(arr));
             Data data = new DataExample(arr, i);
             pc.addData(data);
         }
